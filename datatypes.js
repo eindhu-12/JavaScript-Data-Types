3e2158de0fff40447e5c40bc5372e4f486d0d767
unknown <rapoluindhu2929@gmail.com>
2024-09-28 18:06:02 +0530

    JavaScript Data Types 18-09

---
+++ datatypes.js
@@ -0,0 +1,28 @@
+console.log("---primitive Datatypes----");
+
+var a="Spring";
+console.log(a);
+console.log("type of vaule--",typeof a);
+var b=20;
+console.log(b);
+console.log("type of vaule--",typeof b);
+var c=20.09;
+console.log(c);
+console.log("type of vaule--",typeof c);
+var d=true
+console.log(d);
+console.log("type of vaule--",typeof d);
+var e=undefined;
+console.log(e);
+console.log("type of vaule--",typeof e);
+var f=null;
+console.log(f);
+console.log("type of vaule--",typeof f);
+
+console.log("---Non-primitive Datatypes----");
+var obj={name:"eindhu",age:6,subobj:{surname:"Rapolu"}}
+console.log(obj);
+console.log("type of vaule--",typeof obj);
+var arr=[1,2,3,4,5,"hello",{name:"helo",age:6},[9,8]]
+console.log(arr);
+console.log("type of vaule--",typeof arr);
